f8275669818f059de50dfe55bfde78457c38e891
Teresa Lenz <teresa.lenz@desy.de>
2014-08-08 11:33:51 +0200

    add DQM files at the end

--- CalibTracker/SiStripChannelGain/test/PCL_Teresa/makeStep4Config_AllSteps.sh
+++ CalibTracker/SiStripChannelGain/test/PCL_Teresa/makeStep4Config_AllSteps.sh
@@ -4,7 +4,7 @@ file=${step}
 
 export filelistName=filelist${file}.txt
 export configfileName=step4_ALCAHARVEST_${file}.py
-export outputRootFile=Gains_Tree_${file}_CL2.root
+export outputRootFile=Gains_Tree_${file}_CL2_New.root
 MYVARS='$filelistName:$configfileName:$outputRootFile'
 
 # put this list into the config file
@@ -16,25 +16,17 @@ shopt -s expand_aliases
 module(){ eval `/usr/bin/modulecmd bash $*`;}
 module use -a /afs/desy.de/group/cms/modulefiles/
 module load cmssw
-cd /nfs/dust/cms/user/tlenz/PixelCalibrationCode/CMSSW_5_3_15/src/CalibTracker/SiStripChannelGain/test/PCL_Teresa 
+SCRIPTPATH=`pwd`
+cd $SCRIPTPATH 
 eval `scramv1 runtime -sh`
 cmsRun ${configfileName}
 
 rm ${filelistName}~
 
 # Delete all DQM files besides the last one
-lastline=$(tac ${filelistName} |egrep -m 1 .)
-aux=${#lastline}
-lastRunNumber=${lastline:aux-39:6}
-i=0
+root -b -l -q addDQMFiles.C'("'${step}'")'
+rm addDQMFiles_C.* 
 while read line
   do
-  runNumber=${line:aux-39:6}
-  if [ "$runNumber" == "$lastRunNumber" ]
-      then
-      echo "break"
-      break
-  fi
-  rm DQM*${runNumber}*
-  i=$(( $i + 1 ))
-  done<$filelistName
+  rm DQM*${line}*
+  done<runlist${step}.txt
