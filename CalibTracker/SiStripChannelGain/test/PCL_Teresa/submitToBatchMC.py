e52d7e07015dc02d47ea2bd6ec62d4b0ee1009f9
Teresa Lenz <teresa.lenz@desy.de>
2014-08-26 22:19:35 +0200

    changes to make script work

--- CalibTracker/SiStripChannelGain/test/PCL_Teresa/submitToBatchMC.py
+++ CalibTracker/SiStripChannelGain/test/PCL_Teresa/submitToBatchMC.py
@@ -5,6 +5,8 @@ import shutil
 
 for dirname in os.walk('MC2012'):
     print dirname[0]
+    runnumber=dirname[0][7:]
+    print runnumber
     #print dirname[0]+'/step4_ALCAHARVEST_MC.py'
     shutil.copy2('step4_ALCAHARVEST_MC.py',dirname[0]+'/step4_ALCAHARVEST_MC.py')
 
@@ -20,8 +22,8 @@ for dirname in os.walk('MC2012'):
     config_file.close()
 
     command_file=open(''+dirname[0]+'/'+'command.txt','w')
-    command_file.write('qsub -cwd -l h_vmem=8G -m ae -q long.q -N step4' + dirname[0] + ' ' + os.getcwd() + '/'+dirname[0]+'/step4Driver.sh')
+    command_file.write('qsub -cwd -l h_vmem=10G -m ae -q long.q -N step_' + runnumber + ' ' + os.getcwd() + '/'+dirname[0]+'/step4Driver.sh')
     command_file.close()
-    print('qsub -cwd -l h_vmem=8G -m ae -q long.q -N step4' + dirname[0] + ' ' + os.getcwd() + '/'+dirname[0]+'/step4Driver.sh')
-    #out = commands.getstatusoutput('qsub -cwd -l h_vmem=8G -m ae -q long.q -N gain' + folder + '_' + str(a) + ' ' + os.getcwd() + '/'+folder+'/'+str(a)+'/cmsDriver.sh')
+    print('qsub -cwd -l h_vmem=10G -m ae -q long.q -N step_' + runnumber + ' ' + os.getcwd() + '/'+dirname[0]+'/step4Driver.sh')
+    out = commands.getstatusoutput('qsub -cwd -l h_vmem=10G -m ae -q long.q -N step_' + runnumber + ' ' + os.getcwd() + '/'+dirname[0]+'/step4Driver.sh')
 
